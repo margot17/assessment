c69314247eff4634af79fa2382654b7d93184e38
Marietta Stasinou <stasinoumar@gmail.com>
2017-01-05 10:25:48 +0000

    Add setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 setup(
     name='greengraph',
-    description='calculate green pixels'
+    description='plots the portion of green pixels between two locations'
     version='0.1',
+    author='Marietta Stasinou'
     packages=find_packages(exclude=['*test']),
-    install_requires = ['argparse']
+    install_requires = ['argparse'
+                       'geopy',
+                       'numby',
+                       'matplotlib']
 
 )
 Writing setup.py
