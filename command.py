dd4f742d6bc46f4e98e11523dd23e87a325ea77f
Marietta Stasinou <stasinoumar@gmail.com>
2017-01-05 18:29:55 +0000

    Argparse

--- command.py
+++ command.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser
-import greengraph
-# The code greengraph should change the same except from the fact
-#that  in _init_ I would add the variable 'steps' fos compatibility with this script
-#also I would change the locations to London and Oxford
+from graph import greengraph
+from map import Map
+from matplotlib import pyplot as plt
+
     def process():
-        parser = argparse.ArgumentParser()
+        parser = ArgumentParser()
         parser.add_argument('-c1','--city1',type=str,help='name of city1')
         parser.add_argument('-c2','--city2', type=str, help='name of city2')
         parser.add_argument('s','--steps',type=int, help='number of steps')
         arguments= parser.parse_args()
         print greengraph(arguments.city1, arguments.city2,
         arguments.steps)
+        g=greengraph(arguments.city1, arguments.city2, arguments.steps)
+        g_between=g.green_between(arguments.steps)
+        
+        plt.plot(g_between)
+            plt.title('Green ratio:')
+            plt.xlabel('Steps')
+            plt.ylabel('Green ratio')
+            plt.savefig('out.png')
 
     if __name__ == "__main__":
     process()
